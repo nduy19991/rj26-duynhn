914a741cf8474984547c73b0e931c4ded07b9b59
Nguyễn [H.N] Duy <rn26-duynhn>
2022-10-31 23:39:36 +0700

    homework

--- 2-example/src/App.tsx
+++ 2-example/src/App.tsx
@@ -14,43 +14,34 @@ interface IUser {
 }
 
 function App() {
-  // const user : IUser = {
-  //   id : 1,
-  //   age : 22,
-  //   name : "Nhất Duy",
-  //   address : "38 Yên Bái",
-  // }
 
   const arr: Array<IUser> = [
     {
       id: 1,
       age: 22,
-      name: "Nhất Duy",
+      name: " Nguyễn Hữu Nhất Duy",
       address: "38 Yên Bái",
     },
 
     {
       id: 2,
       age: 25,
-      name: "Văn Việt",
+      name: "Đỗ Hoàng",
       address: "72 Nguyễn Tri Phương",
     },
 
     {
       id: 3,
       age: 21,
-      name: "Ngọc Hùng",
+      name: "Đàm Ngọc Hưng",
       address: "101 Hùng Vương",
     },
   ];
   return (
-    <div>
+    <div className="all">
       <div className="main">
         <h1 className="head">My Information</h1>
-        {/* <img className='img' src={nd} alt=""/>
-      <p>Age : {user.age}</p>
-      <p>Name : {user.name}</p>
-      <p>Address : {user.address}</p> */}
+        <hr />
         {arr?.map((user : IUser)=>{
           return (
             <div className="bg" key={user.id}>
@@ -58,10 +49,11 @@ function App() {
               <img className='img' src={nd} alt=""/>
               </div>
               <p>No : {user.id}</p>
-              <p>Age : {user.age}</p>
               <p>Name : {user.name}</p>
+              <p>Age : {user.age}</p>
               <p>Address : {user.address}</p>
               <hr></hr>
+              
             </div>
           );
         })}
